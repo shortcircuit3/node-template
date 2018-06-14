cc0b26cd59a12129d07e331c7d581a7872615978
alex miles <alex@studiomile.com>
2018-06-14 18:49:55 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -2,8 +2,8 @@ import request from 'supertest';
 import app from './app';
 
 describe('Test the root path', () => {
-    test('It should response the GET method', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-    });
+  test('It should response the GET method', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
 })
