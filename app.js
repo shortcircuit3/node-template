073ead21c7c4f847f8d55e220bab3e63a0753cd6
Alex Miles <alex@studiomile.com>
2018-06-16 15:13:47 +0200

    Add new endpoints

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import express from 'express'
+import request from 'superagent'
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!')
+  const response = { message: 'Hello from our api!' }
+  res.json(response)
+})
+
+app.get('/new-endpoint', (req, res) => {
+  const response = { message: 'This is our new endpoint' }
+  res.json(response)
+})
+
+app.get('/fetch-data', async (req, res) => {
+  try {
+    const response = await request.get('https://dog.ceo/api/breeds/image/random')
+    res.json(response.body)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 module.exports = app
-
